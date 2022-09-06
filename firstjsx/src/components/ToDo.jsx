b9700c2b306bc7d3135899bfaba22a1ba1e3bffd
connorbrennan1212 <connorbrennan1212@gmail.com>
2022-09-05 21:26:57 -0500

    adf

---
+++ firstjsx/src/components/ToDo.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+class ToDoList extends Component{
+    constructor(props){
+        super(props);
+
+    }
+    render(){
+        return(
+            <div>
+                <h1>Hello Dojo!</h1>
+                <h2>Things I need to do:</h2>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marthon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </div>
+        )
+    }
+}
