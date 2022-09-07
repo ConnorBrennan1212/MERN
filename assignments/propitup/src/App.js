9308035f4a8aabfe2a6759dcadcef78aa0555484
connorbrennan1212 <connorbrennan1212@gmail.com>
2022-09-07 15:28:51 -0500

    prop it up and putting together

--- assignments/propitup/src/App.js
+++ assignments/propitup/src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './App.css';
 import PersonComponent from './component/PersonComponent';
 
-var peopleArr = [
-  {"firstName":"Jane","lastName":"Doe","age":45, "hairColor":"Black"},
-  {"firstName":"John","lastName":"Smith","age":88, "hairColor":"Brown"},
-  {"firstName":"Millard","lastName":"Fillmore","age":50, "hairColor":"Brown"},
-  {"firstName":"Maria","lastName":"Smith","age":62, "hairColor":"Brown"}
-]
 
 function App() {
   return (
-    <div className='App'>
-
-      {peopleArr.map(person => {
-        return <PersonComponent firstName ={person.firstName} lastName = {person.lastName} age={person.age} hairColor={person.hairColor}/>
+    <div className="App">
+      <PersonComponent firstName = {"Tommy"} lastName = {"Pickles"} age = {13} hairColor = {"Purple"}/>
+      <PersonComponent firstName = {"Eliza"} lastName = {"Thornberry"} age = {9} hairColor = {"Red"}/>
+      <PersonComponent firstName = {"Harry"} lastName = {"Potter"} age = {17} hairColor = {"Brown"}/>
+      <PersonComponent firstName = {"Sabrina"} lastName = {"Spellman"} age = {16} hairColor = {"Blonde"}/>
     </div>
   );
 }
--- assignments/propitup/src/component/PersonComponent.js
+++ assignments/propitup/src/component/PersonComponent.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 
 class PersonComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+    
     render(){
         return(
             <div>
                 <h1>{this.props.lastName},{this.props.firstName}</h1>
                 <p>Age: {this.props.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button style={btnStyle} onClick={ () => { this.setState({age: this.state.age + 1})} }>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         )
     }
